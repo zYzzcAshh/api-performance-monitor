1c78899b09b736038e28bc10dd0ed06d2b30bb24
zYzzcAshh <zyzzcashh@gmail.com>
2026-03-20 17:46:34 +0000

    refactor: Remove unused verifyToken method from JwtService; streamline JWT handling

--- backend/server/src/main/kotlin/pt/isel/api_pm/service/JwtService.kt
+++ backend/server/src/main/kotlin/pt/isel/api_pm/service/JwtService.kt
@@ -2,7 +2,6 @@ package pt.isel.api_pm.service
 
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
-import com.auth0.jwt.interfaces.DecodedJWT
 
 class JwtService {
     private val secret = "api-pm-secret"
@@ -19,11 +18,4 @@ class JwtService {
             .withExpiresAt(java.util.Date(now + validity))
             .sign(algorithm)
     }
-
-    fun verifyToken(token: String): DecodedJWT =
-        JWT
-            .require(algorithm)
-            .withIssuer(issuer)
-            .build()
-            .verify(token)
 }
