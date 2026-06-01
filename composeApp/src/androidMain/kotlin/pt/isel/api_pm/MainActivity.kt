13d2c5a0ee9231b055f1fdc16b5ec1afe3b8fec8
xicoooooo <faragaodias@gmail.com>
2026-06-01 16:12:22 +0100

    docs upgrade

--- composeApp/src/androidMain/kotlin/pt/isel/api_pm/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/pt/isel/api_pm/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import pt.isel.api_pm.app.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
