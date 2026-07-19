f96b56d04f11db179d22ff26e32bdb50bad887b3
zYzzcAshh <zyzzcashh@gmail.com>
2026-07-19 21:00:46 +0100

    fix: front end connection

--- shared/src/commonMain/kotlin/pt/isel/api_pm/api/ApiConfig.kt
+++ shared/src/commonMain/kotlin/pt/isel/api_pm/api/ApiConfig.kt
@@ -1,5 +1,13 @@
 package pt.isel.api_pm.api
 
 object ApiConfig {
-    const val BASE_URL = "http://localhost:8080"
+    const val isLocalhost = false
+    var BASE_URL = "http://localhost:8080"
+
+    init {
+        // Bad hardcoded approach
+        if (!isLocalhost) {
+            BASE_URL = "https://api-performance-monitor-cnyy.onrender.com/"
+        }
+    }
 }
