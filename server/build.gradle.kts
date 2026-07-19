e9e55f0d18f4e09ba2720a0ef920c3856f7ddb2f
zYzzcAshh <zyzzcashh@gmail.com>
2026-07-19 19:48:58 +0100

    fix: fix server build.gradle.kts

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 group = "pt.isel.api_pm"
 version = "1.0.0"
 application {
-    mainClass.set("pt.isel.api_pm.ApplicationKt")
+    mainClass.set("pt.isel.api_pm.App.ApplicationKt")
 
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
